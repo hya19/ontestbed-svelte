7dcb0b93c161e4df4851de25108d848c8a96fb94
Jung-Hyeon Kim <121010242+naon1735@users.noreply.github.com>
2024-08-20 21:53:55 +0900

    update 2 - form test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,5 +11,16 @@
 
 <!-- 3번 : 본문 -->
 <div class="text-black w-11/12 bg-rose-50 text-xl rounded-2xl text-center align-middle shadow-xl"><br><br>
-    text
+    input 테스트용 쓰지마세요.
+    <form name="contact" netlify>
+        <p>
+          <label>이름 <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>이메일 <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <button type="submit">발신</button>
+        </p>
+      </form>
 <br><br><br></div>
